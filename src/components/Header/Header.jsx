1d66e268af50886c32fb986223c4a084509510ea
Chamil <chamvagapov@yahoo.fr>
2020-05-05 20:39:27 +0200

    utilisation des modules.css rangement des fichier jsx et css creation des dossier

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from'./Header.module.css';
 
 const Header = () => {
-   return <header className='header'>
+   return <header className={s.header}>
     <img src="https://about.canva.com/wp-content/uploads/sites/3/2016/08/logos-1.png"></img>
     </header>
 }
---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './Post.module.css';
+
+const Post = () => {
+    return (
+
+        <div className={s.item}>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcThuB5b0TO6F40mF3AZ0bvBXpCiwXuJLbTb9W2a_UPBBnh3A7mN&usqp=CAU' />
+            post 1
+
+        <div>
+                <span>like</span>
+            </div>
+        </div>
+    )
+}
+
+export default Post;
