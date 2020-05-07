4f70fae3a9730d0b528b9b9fc1ec3be6c8583c6d
Chamil <chamvagapov@yahoo.fr>
2020-05-07 18:58:58 +0200

    integration de quelque props

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
 
         <div className={s.item}>
             <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcThuB5b0TO6F40mF3AZ0bvBXpCiwXuJLbTb9W2a_UPBBnh3A7mN&usqp=CAU' />
-            post 1
+           { props.message}
 
         <div>
-                <span>like</span>
+            <span>{props.like}</span>
+                
             </div>
         </div>
     )
