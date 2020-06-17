ce0038561a5990a30c04a45c9fee92a4ac5e763b
Chamil <chamvagapov@yahoo.fr>
2020-06-17 12:00:52 +0200

    utilisation de la function pure & création de la class component extends React.Component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,35 +3,38 @@ import s from './users.module.css';
 import * as axios from 'axios';
 import userPhoto from '../../assets/images/usersImg.png'
 
-let Users = (props) => {
+class Users extends React.Component {
 
-    if (props.users.length === 0) {
+    constructor(props) {
+        super(props);
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response =>{
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                this.props.setUsers(response.data.items);
+            });
+    }
 
-            props.setUsers(response.data.items);
-
-    });
-
-}
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
+    render() { // il faut toujour utiliser render pour retourner le jsx
+        return <div>
+            <button onClick={this.getUsers}>Get users</button>
+            {/*appelle de la function pure*/}
+            {
+                this.props.users.map(u => <div key={u.id}>
             <span>
                 <div>
-                    <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto} alt={'kratos'}/>
+                    <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto}
+                         alt={'kratos'}/>
                 </div>
                 <div>
                     {u.followed
                         ? <button onClick={() => {
-                            props.unfollow(u.id)
+                            this.props.unfollow(u.id)
                         }}>Unfollow</button>
                         : <button onClick={() => {
-                            props.follow(u.id)
+                            this.props.follow(u.id)
                         }}>Follow</button>}
                 </div>
             </span>
-                <span>
+                    <span>
                 <span>
                     <div>{u.name}</div>
                     <div>{u.status}</div>
@@ -41,9 +44,10 @@ let Users = (props) => {
                     {/*<div>{u.location.city}</div>*/}
                 </span>
             </span>
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+    }
 }
 
 export default Users;
